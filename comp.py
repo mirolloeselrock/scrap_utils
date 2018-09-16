eba64188dbe5623f680ac27829abc2a377b02069
mirolloeselrock <2404336+mirolloeselrock@users.noreply.github.com>
2018-09-16 14:15:05 +0200

    solucionados problemillas con linux

--- comp.py
+++ comp.py
@@ -3,13 +3,12 @@ import os
 import re
 import ConfigParser
 
-
 def getLength(ffmpeg_path, input_video):
-    process = subprocess.Popen([ffmpeg_path + " ",  '-i', input_video], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    process = subprocess.Popen(ffmpeg_path + " " + "-i " + '"' + input_video + '"', stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
     stdout, stderr = process.communicate()
     matches = re.search(r"Duration:\s{1}(?P<hours>\d+?):(?P<minutes>\d+?):(?P<seconds>\d+\.\d+?),", stdout, re.DOTALL).groupdict()
     return float(matches['seconds']) + 60 * float(matches['minutes']) + 3600 * float(matches['minutes'])
-    
+
 def compressFiles():
     config = ConfigParser.RawConfigParser()   
     config.read("comp.cfg")
@@ -31,13 +30,12 @@ def compressFiles():
                 length = DURACION_MAXIMA
             os.rename(file, file + ".bak")
             input = '-sseof -{0} -i "./{1}.bak" -profile:v high -level 3.1 -vcodec libx264 -crf {2} -vf "format=yuv420p,scale={3}:{4}:force_original_aspect_ratio=decrease,pad={3}:{4}:(ow-iw)/2:(oh-ih)/2,setsar=1" {5} -preset {6} -threads {7} "./{8}"'.format(length, file, FFMPEG_CRF, RESOLUCION_ANCHO, RESOLUCION_ALTO, soundStr, FFMPEG_PRESET, FFMPEG_HILOS, file)
-            cmd = FFMPEG_PATH + " " + input
-            
-            p = subprocess.Popen(cmd , shell=True)
-            p.wait()        
+            cmd = FFMPEG_PATH + " " + input     
+            p = subprocess.Popen(cmd, shell=True)
+            p.wait()
 
 def main():
     compressFiles()
-    
+
 if __name__ == "__main__":
     main()
